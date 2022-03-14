ff043e853d9897dff4b70a9af50e3504c882c7d7
Davide Preatoni <46676102+DavidePrea@users.noreply.github.com>
2022-03-14 20:51:06 +0100

    Bag - Tower - TowerColor

---
+++ src/main/java/it/polimi/ingsw/Model/Bag.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.Model;
+
+import java.util.ArrayList;
+
+public class Bag {
+
+   ArrayList<Student> students = new ArrayList<>();
+
+
+   public ArrayList<Student> getStudents() {
+      return students;
+   }
+
+   public void setStudents(ArrayList<Student> students) {
+      this.students = students;
+   }
+}
+
