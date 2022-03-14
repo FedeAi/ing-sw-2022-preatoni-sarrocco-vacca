b1b8c2e8982557430ef8d238e9fe782d95b7cf1f
Alessandro Vacca <alessandro2.vacca@mail.polimi.it>
2022-03-14 20:22:33 +0100

    Init Color, Professor, Student classes

---
+++ src/main/java/it/polimi/ingsw/Model/Professor.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.Model;
+
+public class Professor {
+    Color color;
+
+    public Color getColor() {
+        return color;
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/Model/Student.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.Model;
+
+public class Student {
+    Color color;
+
+    public Color getColor() {
+        return color;
+    }
+}
