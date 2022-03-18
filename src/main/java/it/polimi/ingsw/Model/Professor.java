edcc0d214089b8ce2a6413f97313ed28702057b7
Alessandro Vacca <alessandro2.vacca@mail.polimi.it>
2022-03-18 18:30:54 +0100

    Create constructors for student, professor classes

--- src/main/java/it/polimi/ingsw/Model/Professor.java
+++ src/main/java/it/polimi/ingsw/Model/Professor.java
@@ -5,6 +5,10 @@ import it.polimi.ingsw.Model.Enumerations.Color;
 public class Professor {
     private Color color;
 
+    public Professor(Color color) {
+        this.color = color;
+    }
+
     public Color getColor() {
         return color;
     }
--- src/main/java/it/polimi/ingsw/Model/Student.java
+++ src/main/java/it/polimi/ingsw/Model/Student.java
@@ -5,6 +5,10 @@ import it.polimi.ingsw.Model.Enumerations.Color;
 public class Student {
     private Color color;
 
+    public Student(Color color) {
+        this.color = color;
+    }
+
     public Color getColor() {
         return color;
     }
