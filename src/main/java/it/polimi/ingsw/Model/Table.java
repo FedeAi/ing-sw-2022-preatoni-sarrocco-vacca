cc6a51fed2cd98a6e2bd6139a2c498bedba97cc9
Alessandro Vacca <alessandro2.vacca@mail.polimi.it>
2022-03-22 00:30:17 +0100

    Created constructor for Table

--- src/main/java/it/polimi/ingsw/Model/Table.java
+++ src/main/java/it/polimi/ingsw/Model/Table.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.Model;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Table {
@@ -8,6 +9,27 @@ public class Table {
     private ArrayList<CharacterCard> characterCards; //array, fixed size = 3
     private Bag bag;
     private Turn turn;
+
+
+    // The table constructor creates the bag with the specified students inside. It then initializes the islands.
+    // Proper testing to be added
+    public Table(int motherNatureIsland) {
+        bag = new Bag(2);
+        for(int i = 0; i < 12; i++) {
+            // Properly treat the MNI's opposite (different init)
+            if(i == motherNatureIsland) {
+                islands.add(new Island());
+                motherNature = new MotherNature(islands.get(i));
+            }
+            else {
+                ArrayList<Student> students = bag.extract(2);
+                islands.add(new Island());
+                islands.get(i).addStudents(students);
+            }
+        }
+        // Recreate the bag with the correct amount of students after the initial setup
+        bag = new Bag(24);
+    }
 
     // Rather than returning the reference to the bag, we expose the extraction of the student itself
     public Bag getBag() {
