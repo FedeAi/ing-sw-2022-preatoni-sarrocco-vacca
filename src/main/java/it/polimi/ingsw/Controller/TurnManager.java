3733f4f87c71a709209647b4e17de1a8f6224adb
Davide Preatoni <46676102+DavidePrea@users.noreply.github.com>
2022-03-25 15:26:51 +0100

    turnowner

--- src/main/java/it/polimi/ingsw/Controller/TurnManager.java
+++ src/main/java/it/polimi/ingsw/Controller/TurnManager.java
@@ -3,6 +3,13 @@ package it.polimi.ingsw.Controller;
 import it.polimi.ingsw.Model.Player;
 
 public class TurnManager {
+
     private GameManager gameManager;
     private Player turnOwner;
+
+    public Player getTurnOwner() {
+        return turnOwner;
+    }
+
 }
+
