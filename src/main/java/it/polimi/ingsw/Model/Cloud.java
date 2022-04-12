1f64c6fd7ac409ad434d983cd66c7ff8eb008ea2
Federico <fede.sarrocco@gmail.com>
2022-04-12 10:36:53 +0200

    Cloud methods ( isEmpty, addStudents, pickStudents)

--- src/main/java/it/polimi/ingsw/Model/Cloud.java
+++ src/main/java/it/polimi/ingsw/Model/Cloud.java
@@ -2,16 +2,31 @@ package it.polimi.ingsw.Model;
 
 import it.polimi.ingsw.Model.Enumerations.Color;
 
+import java.util.EnumMap;
 import java.util.Map;
 
 public class Cloud {
     private Map<Color, Integer> students;
 
-    public Map<Color, Integer> getStudents() {
-        return students;
+    public Map<Color, Integer> pickStudents() {
+        Map<Color, Integer> returnStudents = students;
+        students = new EnumMap<Color, Integer>(Color.class);
+        return returnStudents;
     }
 
-    public void addStudents(Color color) {
+    public boolean isEmpty(){
+        return students.size()==0;
+    }
+
+    public void addStudent(Color color) {
         students.put(color, students.getOrDefault(color, 0) + 1);
     }
+
+    public void addStudents(Map<Color, Integer> addStudents) {
+        if(students.size()>0){
+            students.forEach(
+                    (key, value) -> addStudents.merge(key, value, Integer::sum));
+        }
+    }
+
 }
