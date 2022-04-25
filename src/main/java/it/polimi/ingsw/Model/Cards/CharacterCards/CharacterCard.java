5e246c28403c9e32c7fbe03f079239b62c139423
Davide Preatoni <46676102+DavidePrea@users.noreply.github.com>
2022-04-25 12:10:16 +0200

    PrincessMove, MinstrelSwap computing influence at the end

--- src/main/java/it/polimi/ingsw/Model/Cards/CharacterCards/CharacterCard.java
+++ src/main/java/it/polimi/ingsw/Model/Cards/CharacterCards/CharacterCard.java
@@ -21,6 +21,7 @@ public abstract class CharacterCard extends Card {
      * this method is called to initialized internal state of the card, as the constructor will not do that
      */
     public abstract void init();
+
     public abstract void activate(Rules rules, Game game);
 
     public abstract void deactivate(Rules rules, Game game);
