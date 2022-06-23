a7c305acdad281b04e3a14d8d17e9b6ac5f0200b
Alessandro Vacca <alessandro2.vacca@mail.polimi.it>
2022-06-23 17:26:02 +0200

    Game launcher added.

--- src/main/java/it/polimi/ingsw/Eriantys.java
+++ src/main/java/it/polimi/ingsw/Eriantys.java
@@ -1,10 +1,42 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.Client.cli.CLI;
+import it.polimi.ingsw.Client.gui.GUI;
+import it.polimi.ingsw.Server.Server;
+
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * Eriantys class is main launcher for the project.
  */
 public class Eriantys {
+    /**
+     * Method main lets the user select the different modes of operation: Client (CLI/GUI) or Server.
+     *
+     * @param args of type String[]
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to Eriantys! What type of instance do you want to launch?");
+        System.out.println("0 - Server");
+        System.out.println("1 - Client with CLI interface");
+        System.out.println("2 - Client with GUI interface");
+        System.out.print(">");
+        Scanner scanner = new Scanner(System.in);
+        int selection = 0;
+        try {
+            selection = scanner.nextInt();
+        } catch (Exception e) {
+            System.err.println("Only numbers are accepted. Exiting...");
+            System.exit(1);
+        }
+        switch (selection) {
+            case 0 -> Server.main(null);
+            case 1 -> CLI.main(null);
+            case 2 -> GUI.main(null);
+            default -> {
+                System.err.println("Wrong selection. Exiting...");
+                System.exit(1);
+            }
+        }
     }
 }
